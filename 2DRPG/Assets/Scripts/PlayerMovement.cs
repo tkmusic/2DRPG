b5eaf1c7b52ec7ea5425133539e92968827e1c23
tkmusic <68860474+tkmusic@users.noreply.github.com>
2021-11-10 15:24:54 +0100

    Added Movement Animation
    -Created Animation files
    -Created Blend Trees
    -Added Animation methods to PlayerMovement.cs

--- 2DRPG/Assets/Scripts/PlayerMovement.cs
+++ 2DRPG/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour{
 
     public float speed;
     private Rigidbody2D myRigidbody;
+    private Animator animator;
 
     private Vector3 change;
     // Start is called before the first frame update
     void Start(){
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -18,15 +20,26 @@ public class PlayerMovement : MonoBehaviour{
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical"); 
+        UpdateAnimationAndMove();
         //Debug.Log(change);
+
+    }
+
+    void UpdateAnimationAndMove(){
         if(change != Vector3.zero){
             MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+        }
+        else {
+            animator.SetBool("moving", false);
         }
     }
 
     void MoveCharacter() {
         myRigidbody.MovePosition(
-            transform.position + change * speed * Time.fixedDeltaTime
+            transform.position + change.normalized * speed * Time.fixedDeltaTime
         );
     }
 }
